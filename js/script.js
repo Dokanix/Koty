4f5dbaeb7673939be002ddb14d91360bafe9238c
Dominik Piotrowicz <dom.piotrowicz@gmail.com>
2020-12-13 14:48:37 +0100

    Fix Linking

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ const catThumbnails = document.querySelectorAll(".c-figure");
 
 for (const cat of catThumbnails) {
   cat.addEventListener("click", (e) => {
-    console.log(e.target.id);
-    window.location.href = `/Koty/${e.target.id}.html`;
+    let url = window.location.href;
+    url = url.split("/");
+    url.pop();
+    url = url.join("/");
+    window.location = `${url}/${e.target.id}.html`;
   });
 }
